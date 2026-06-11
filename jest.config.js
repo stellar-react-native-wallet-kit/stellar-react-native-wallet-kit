ce774f7ea5553887f80fd13a270bd3bfa4d71037
tech-adrian <a.emmanuelcaxton@gmail.com>
2026-06-11 22:42:26 +0100

    Add project configuration files

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+  setupFilesAfterEnv: [],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@walletconnect/react-native-compat)/)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/__tests__/**',
+    '!src/index.ts',
+  ],
+};
